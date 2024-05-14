bcf76258c25250eda81e143b962c4df72499010d
MeirelesJv <meirelesjv@hotmail.com>
2024-05-13 21:16:09 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+function clicar(clickedElement) {
+    // Check if the clicked element already has the 'teste' class
+    const hasTesteClass = clickedElement.classList.contains('devActiv');
+
+    // If it does, remove the class
+    if (hasTesteClass) {
+        clickedElement.classList.remove('devActiv');
+        return; // Exit the function if the class is removed
+    }
+
+    // Otherwise, remove the class from all other elements and add it to the clicked element
+    const allDevElements = document.querySelectorAll('.dev');
+    allDevElements.forEach(element => element.classList.remove('devActiv'));
+    clickedElement.classList.add('devActiv');
+};
+
+function truncateParagraphs() {
+    const allParagraphs = document.querySelectorAll('p'); // Select all <p> elements
+    allParagraphs.forEach(paragraph => {
+      const textContent = paragraph.textContent;
+      if (textContent.length > 100) {
+        paragraph.textContent = textContent.substring(0, 100) + '...';
+      }
+    });
+}
+function checkScreenSize() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 768) { // Adjust breakpoint as needed
+        truncateParagraphs();
+    }else{
+        truncateParagraphs(0);
+    }
+}
